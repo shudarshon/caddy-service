e84aab7db348f4907211e12daa9f69da31c114bc
shudarshon <shudarshon@upskillable.com>
2018-02-16 01:46:48 +0600

    added systemd script and update readme

---
+++ README.md
@@ -0,0 +1,4 @@
+## Run caddy with systemd, supervisor ##
+
+Follow instructions in different folder. Don't forget to create issues and
+contribute!
---
+++ systemd/provision.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Create necessary directories for caddy
+sudo mkdir /var/www /etc/caddy /etc/ssl/caddy
+
+#Install Caddy
+curl https://getcaddy.com | bash -s personal http.cache,http.expires,http.git,http.minify,http.nobots,http.proxyprotocol,http.realip,http.upload
+
+#Allow caddy to use port 80,443
+sudo setcap cap_net_bind_service=+ep `which caddy`
+
+# Create Caddy config file
+sudo touch /etc/caddy/Caddyfile
+
+#Change permission and ownership
+sudo chown -R  www-data:www-data /var/www
+sudo chown -R  www-data:root /etc/ssl/caddy
+sudo chown root:www-data /etc/caddy
+
+#Copy caddy systemd script and restart service
+sudo cp caddy.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable caddy.service
+sudo systemctl start caddy.service
